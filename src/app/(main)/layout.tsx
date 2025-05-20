1169c1a761878135f6c42ea3f821a82ae6751c79
Tranvu <huynhdong1115@gmail.com>
2025-05-21 00:29:45 +0700

    test

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
       <main className="container flex flex-grow px-4 mx-auto sm:px-6 lg:px-8">
         {children}
