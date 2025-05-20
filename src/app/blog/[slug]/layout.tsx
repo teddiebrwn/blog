8d8d373d80fa59e9272f1adac87c6bb857a4b508
Tranvu <huynhdong1115@gmail.com>
2025-05-21 00:37:41 +0700

    test3

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -6,7 +6,7 @@ export default function BlogPostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative">
+    <div className="relative mx-5">
       <div className="my-6">
         <FontThemeWithScroll />
       </div>
